bf6d004be469b7135bb15e79f6e718be9757b686
shixy96 <shixy@feifanuniv.com>
2019-07-24 14:59:17 +0800

    add fake data

---
+++ http-api/model/x/shareInfo.js
@@ -0,0 +1,69 @@
+module.exports = [
+  {
+    id: "teacher",
+    schoolId: 0,
+    contentId: 1,
+    contentType: 10,
+    createTime: Date().now(),
+    referCampaign: "5b7fd723eb3a9440d057467d",
+    title: "教师资格证",
+    tags: ["tag1", "tag2"],
+    free: true,
+    structureInfo: {
+      catalogId: "xxx",
+      classId: 111,
+      courseId: 111,
+      knowledgeId: 111
+    },
+    promotionCode: 456
+  },
+  {
+    id: "english",
+    schoolId: 0,
+    contentId: 1,
+    contentType: 10,
+    createTime: Date().now(),
+    referCampaign: "5b7fd723eb3a9440d057467c",
+    title: "学位英语",
+    tags: ["tag1", "tag2"],
+    free: true,
+    structureInfo: {
+      catalogId: "xxx",
+      classId: 111,
+      courseId: 111,
+      knowledgeId: 111
+    },
+    promotionCode: 456
+  },
+  {
+    id: "degree",
+    schoolId: 0,
+    contentId: 1,
+    contentType: 15,
+    createTime: Date().now(),
+    referCampaign: "5b7fd723eb3a9440d057467b",
+    title: "学位英语",
+    tags: ["tag1", "tag2"],
+    free: true,
+    structureInfo: {
+      catalogId: "5b7fd723eb3a9440d0574654",
+      classId: 111,
+      courseId: 111,
+      knowledgeId: 111
+    },
+    promotionCode: 456
+  },
+  {
+    id: "live",
+    schoolId: 0,
+    contentId: 1,
+    contentType: 19,
+    createTime: Date().now(),
+    referCampaign: null,
+    title: "学位英语公开课",
+    tags: ["tag1", "tag2"],
+    free: null,
+    structureInfo: null,
+    promotionCode: null
+  }
+];
