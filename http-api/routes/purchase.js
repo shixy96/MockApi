52768c99e14c24087bb2da3ff32c2fec6e399c31
shixy96 <shixy@feifanuniv.com>
2019-08-15 17:44:30 +0800

    add purchaseEntryCourse

--- http-api/routes/purchase.js
+++ http-api/routes/purchase.js
@@ -2,11 +2,24 @@ module.exports = app => ({
   "get /history": app.model.purchase.history["v6.3"],
 
   "get /getOffers": ctx => {
-    const { promotionCode } = ctx.request.query;
-    let result = ctx.model.purchase.offer.offer;
-    if (promotionCode) {
-      result.data.push(ctx.model.purchase.offer.promotion);
+    let offerTmp = app.model.purchase.offer.mechanism;
+    let result = [];
+    let { "catalogIds[]": catalogIds } = ctx.request.query;
+    if (typeof catalogIds === "string") {
+      catalogIds = [catalogIds];
     }
+    catalogIds.forEach(catalogId => {
+      let offer = JSON.parse(JSON.stringify(offerTmp));
+      offer.id = Date.now();
+      offer.catalogId = catalogId;
+      result.push(offer);
+    });
+    result = {
+      data: result,
+      extraData: null,
+      hr: 0,
+      message: "成功"
+    };
     ctx.body = result;
   }
 });
