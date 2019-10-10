3d5834c42938770b74f776fa9704ed9d3a1de96e
shixy96 <shixy@feifanuniv.com>
2019-10-10 10:37:31 +0800

    chore(mock api): Move folder

--- http-api/config/config.js
+++ http-api/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 3000,
-  version: "/v7_1",
+  version: "/v7_2",
   apiPath: {
     dev: "http://m.dev.qingshuxuetang.com/qingshu-api",
     ppe: "http://m.ppe.qingshuxuetang.com/qingshu-api"
---
+++ http-api/model/test/README.md
@@ -0,0 +1,3 @@
+### This folder is used as the inject api data for unit testing of msite
+
+[msite unit test](../../../../qingshu/test/unit)
