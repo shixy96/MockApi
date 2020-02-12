60383aa3ec3bf0cfd7d1cdc9350b0e730eb1fd7a
tangyh <tangyh@feifanuniv.com>
2020-02-12 09:58:38 +0800

    feature(msite)-qsc12079: studyRecord

--- http-api/routes/reward.js
+++ http-api/routes/reward.js
@@ -1,5 +1,7 @@
 const mock = require("../app/mock-api");
 
 module.exports = (app = new mock()) => ({
-  "get /mine": app.model.reward.mine
+  "get /mine": app.model.reward.mine,
+
+  "get /checkin": app.model.reward.checkin,
 });
