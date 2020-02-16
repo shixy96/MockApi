3e936188898b30b146d863148097dc9abcc9b967
tangyh <tangyh@feifanuniv.com>
2020-02-16 14:16:48 +0800

    feature(msite)-QSC-12081-taskDetail entrance

--- http-api/routes/reward.js
+++ http-api/routes/reward.js
@@ -4,4 +4,6 @@ module.exports = (app = new mock()) => ({
   "get /mine": app.model.reward.mine,
 
   "get /checkin": app.model.reward.checkin,
+
+  "get /scheduleSearch": app.model.reward.scheduleSearch
 });
