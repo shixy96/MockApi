8e6753a6f8905324931ee757274a75043bfd5a49
tangyh <tangyh@feifanuniv.com>
2020-04-09 20:37:19 +0800

    feat(msite)-qsc14128 & qsc14129

--- http-api/routes/chatroom.js
+++ http-api/routes/chatroom.js
@@ -1,5 +1,11 @@
 const mock = require("../app/mock-api");
 
 module.exports = (app = new mock()) => ({
-  "get /getMessage": app.model.chatroom.message
+  "get /getMessage": app.model.chatroom.message,
+
+  "get /interaction/detail": app.model.interaction.detail,
+
+  "get /interaction/getPresenceDetail": app.model.interaction.getPresenceDetail,
+  
+  "post /interaction/submit": app.model.interaction.submit
 });
