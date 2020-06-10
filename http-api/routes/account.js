0d7e1d0de5ee92c8f94cc8d7b23b3191a87251a2
tangyh <tangyh@feifanuniv.com>
2020-06-10 15:24:19 +0800

    feat(msite)-qsc16254:  account cancellation

--- http-api/routes/account.js
+++ http-api/routes/account.js
@@ -3,5 +3,8 @@ const mock = require("../app/mock-api");
 module.exports = (app = new mock()) => ({
   "get /queryBalance": ctx => {
     ctx.body = app.model.account.queryBalance;
+  },
+  "post /deleteUser": ctx => {
+    ctx.body = app.model.common.noneDataResult;
   }
 });
